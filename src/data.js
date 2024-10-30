75806a7918b884cd937d4d6dae24c916ea17d44c
Sparkoms <sparkoms@gmail.com>
2024-10-30 16:56:15 +0300

    Data

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const cart = [
+    {
+        id: 1,
+        img: 'macbook.jpg',
+        title: 'Apple MacBook Air 13',
+        count: 1,
+        price: 110000,
+        priceTotal: 110000,
+    },
+    {
+        id: 2,
+        img: 'apple-watch.jpg',
+        title: 'Apple watch',
+        count: 1,
+        price: 29000,
+        priceTotal: 29000,
+    },
+    {
+        id: 3,
+        img: 'macbook.jpg',
+        title: 'Apple MacBook Air 13',
+        count: 1,
+        price: 110000,
+        priceTotal: 110000,
+    }, 
+]
